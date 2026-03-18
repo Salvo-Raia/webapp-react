5417cd9820ad83ad43039f17340f7fc77a0e78c7
Salvo-Raia <salvo.raia@outlook.it>
2026-03-19 00:06:07 +0100

    Install bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
+// Imports
 import App from "./App.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // Custom CSS
 import "./assets/css/index.css";
-// Bootstrap CSS
-
+// Bootstrap CSS and icons
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 //Bootstrap script
+import "bootstrap/dist/js/bootstrap.min.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
