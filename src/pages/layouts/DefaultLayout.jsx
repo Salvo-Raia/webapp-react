92752eefef7f627afc12c94feb05032ec5c9fb0d
Salvo-Raia <salvo.raia@outlook.it>
2026-03-19 00:48:30 +0100

    Customize navbar

--- src/pages/layouts/DefaultLayout.jsx
+++ src/pages/layouts/DefaultLayout.jsx
@@ -4,54 +4,38 @@ import { NavLink, Outlet } from "react-router-dom";
 export default function DefaulLayout() {
   return (
     <>
-      <header>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <NavLink className="navbar-brand" href="#">
-              Navbar
-            </NavLink>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <NavLink
-                    className="nav-link active"
-                    aria-current="page"
-                    href="#"
-                  >
-                    Home
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" href="#">
-                    Features
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" href="#">
-                    Pricing
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link disabled" aria-disabled="true">
-                    Disabled
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <NavLink to="/" className="navbar-brand">
+            Site Logo
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse " id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link active" aria-current="page">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="movies" className="nav-link">
+                  Movie List
+                </NavLink>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </header>
+        </div>
+      </nav>
       <main>
         <Outlet />
       </main>
