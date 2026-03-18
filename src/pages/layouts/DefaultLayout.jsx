66332ebc2c9ae5cfa6ad934cb14e19c8d86ae4f7
Salvo-Raia <salvo.raia@outlook.it>
2026-03-19 00:43:34 +0100

    Add: navbar

--- src/pages/layouts/DefaultLayout.jsx
+++ src/pages/layouts/DefaultLayout.jsx
@@ -1,10 +1,57 @@
 // Imports
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function DefaulLayout() {
   return (
     <>
-      <header>Navbar</header>
+      <header>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <NavLink className="navbar-brand" href="#">
+              Navbar
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink
+                    className="nav-link active"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" href="#">
+                    Features
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" href="#">
+                    Pricing
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link disabled" aria-disabled="true">
+                    Disabled
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
       <main>
         <Outlet />
       </main>
