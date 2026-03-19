a4868b401e1eb4fff53ef3875fe20a7c25b71521
Salvo-Raia <salvo.raia@outlook.it>
2026-03-19 01:24:00 +0100

    Add: useState

--- src/pages/movies/MovieList.jsx
+++ src/pages/movies/MovieList.jsx
@@ -3,10 +3,13 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function MovieList() {
+  const [movies, setMovies] = useState([]);
   useEffect(fetchMovies, []);
 
   function fetchMovies() {
-    axios.get("http://localhost:3000/movies");
+    axios.get("http://localhost:3000/movies").then((res) => {
+      setMovies(res.data.result);
+    });
   }
 
   return <h1>Movie List</h1>;
