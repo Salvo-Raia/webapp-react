c41c397b15f224d1acdd34922bcd9e859248d755
Salvo-Raia <salvo.raia@outlook.it>
2026-03-19 18:24:34 +0100

    Add: fetchMovie to MovieDetail.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,13 +5,14 @@ export default function MovieCard({ movie }) {
   return (
     <div className="card">
       <img src={movie.image} className="card-img-top" alt={movie.title} />
-      <div className="card-body">
-        <h5 className="card-title">{movie.title}</h5>
-        <p className="mb-1">{movie.genre}</p>
-        <p>
-          Directed by: <em>{movie.director}</em>
-        </p>
-        <p>"{movie.abstract}"</p>
+      <div className="card-body bg-dark text-light">
+        <h3 className="card-title">{movie.title}</h3>
+        <h6 className="text-secondary my-1">{movie.genre}</h6>
+        <address className="my-2">
+          Directed by: <em>{movie.director}</em> - {`(${movie.release_year})`}
+        </address>
+
+        <p className="my-2">"{movie.abstract}"</p>
         <p className="card-text">Vote: {movie.average_vote}/5</p>
         <Link to={"/movies/" + movie.id}>See more</Link>
       </div>
--- src/pages/movies/MovieDetail.jsx
+++ src/pages/movies/MovieDetail.jsx
@@ -1,3 +1,17 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function MovieDetail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState();
+  useEffect(fetchMovie, []);
+
+  function fetchMovie() {
+    axios.get(`http://localhost:3000/movies/${id}`).then((res) => {
+      setMovie(res.data.result);
+    });
+  }
+
   return <h1>Movie Detail</h1>;
 }
